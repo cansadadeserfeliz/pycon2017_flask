1cc0d2a33fa9994bed52316f354c27a04829b495
Vera Mazhuga <vmazhuga@tappsi.co>
2017-02-07 09:40:25 -0500

    Vistas

--- users/views.py
+++ users/views.py
@@ -1,10 +1,22 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
-users = Blueprint(
-    'users',
-    __name__,
-    template_folder='templates')
+users = Blueprint('users', __name__, template_folder='templates')
 
 @users.route('/')
 def hello():
     return 'Hello Worlds!'
+
+
+@users.route('/user/<int:pk>')
+def user_detail(pk):
+    print(request)
+    print(request.method)  # GET
+    print(request.args)    # ImmutableMultiDict([('q', u'foo')])
+    print(request.headers)
+    return '<h1>Hello, user #{}!</h1>'.format(pk)
+
+
+@users.route('/user/<int:pk>')
+#@login_required
+def user_detail(pk):
+    return '<h1>Hello, user #{}!</h1>'.format(pk)
