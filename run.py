d98eebcb223190ede621d93b08129646df23c378
Vera Mazhuga <vmazhuga@tappsi.co>
2017-02-07 10:23:15 -0500

    Procesadores de contexto

--- run.py
+++ run.py
@@ -2,11 +2,19 @@ from flask import Flask
 
 
 def create_app():
-    app = Flask(__name__,
-        static_url_path='/static')
+    app = Flask(__name__, static_url_path='/static')
     app.config.from_object('conf.config')
 
     from users.views import users
     app.register_blueprint(users, url_prefix='/users')
 
+    @app.context_processor
+    def constants_processor():
+        return {
+            'say_hello': 'Hola',
+        }
+
     return app
+
+
+
